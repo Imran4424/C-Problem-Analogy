ec3d12b873473a069455b895b3dba051fd707379
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-02 14:46:50 +0600

    init repo

--- 10. One Dimentional Array/10.1.c
+++ 10. One Dimentional Array/10.1.c
@@ -6,15 +6,18 @@
 
 int main(int argc, char const *argv[])
 {
+	printf("Enter the size of the array\n");
 	int arSize;
 	scanf("%d", &arSize);
 
 	int arr[arSize];
 
+	printf("enter %d int number\n", arSize);
 	for(int i = 0; i < arSize; i++) {
 		scanf("%d", &arr[i]);
 	}
 
+	printf("The array contains\n");
 	for(int i = 0; i < arSize; i++) {
 		printf("%d ", arr[i]);	
 	}
