da84b660eff280421e40273e6bd5b3fa06b07caa
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-02 14:45:05 +0600

    init repo

--- 10. One Dimentional Array/10.1.c
+++ 10. One Dimentional Array/10.1.c
@@ -1,3 +1,25 @@
 /*
 	10.1. Write a program that read and display an array
 */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int arSize;
+	scanf("%d", &arSize);
+
+	int arr[arSize];
+
+	for(int i = 0; i < arSize; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for(int i = 0; i < arSize; i++) {
+		printf("%d ", arr[i]);	
+	}
+
+	printf("\n");
+
+	return 0;
+}
