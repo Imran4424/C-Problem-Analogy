0150c805b0301c34ac394d00d30bd836eda2922d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-02 14:33:12 +0600

    init repo

--- 8.Loop/8.1.c
+++ 8.Loop/8.1.c
@@ -2,3 +2,20 @@
 	8.1. Write down a program that can take a lower bound i and an upper bound n and then 
 	find out the summation of those numbers which are even from i to n.
 */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int i, n, sum = 0;
+
+	scanf("%d %d", &i, &n);
+
+	for(int traveler = i; traveler <= n; traveler++) {
+		sum += traveler;
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
